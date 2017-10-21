1cbd45c364fd456968520d9d91c39cebe15cfc4e
irhamputra <irhamputraprasetyo@gmail.com>
2017-10-21 13:53:41 +0200

    load data in componentWillMount

--- client/main.js
+++ client/main.js
@@ -1,23 +1,32 @@
 // #1 Import React lib
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
-// Axios Http GET
-
 // #2 Create Component
-const App = () => {
-    return (
-        <div>
-            <ImageList/>
-        </div>
-    )
-};
+class App extends Component {
+
+    constructor(){
+        super()
+    }
+
+    componentWillMount(){
+        // Axios HTTP Get
+        axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+            .then(response => console.log(response))
+    }
+
+    render(){
+        return (
+            <div>
+                <ImageList/>
+            </div>
+        )
+    }
+}
 
 // #3 Render Component
 Meteor.startup(() => {
     ReactDOM.render(<App />, document.querySelector('.container'));
-    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-        .then(response => console.log(response))
 });
