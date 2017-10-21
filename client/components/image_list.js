7ece682448698c325fc648a585e70103f285512a
irhamputra <irhamputraprasetyo@gmail.com>
2017-10-21 13:45:14 +0200

    axios http get from API Endpoint

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -12,11 +12,11 @@ const IMAGES = [
 // #2 Creating component
 const ImageList = () => {
     const RenderedImages = IMAGES.map((image) => {
-       return <ImageDetail image={image}/>
+       return <ImageDetail key={image.title} image={image}/>
     });
 
     return (
-        <ul>
+        <ul className="media-list list-group">
             {RenderedImages}
         </ul>
     )
--- client/main.js
+++ client/main.js
@@ -1,13 +1,15 @@
 // #1 Import React lib
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
+
+// Axios Http GET
 
 // #2 Create Component
 const App = () => {
     return (
         <div>
-            <h1>Image List:</h1>
             <ImageList/>
         </div>
     )
@@ -16,4 +18,6 @@ const App = () => {
 // #3 Render Component
 Meteor.startup(() => {
     ReactDOM.render(<App />, document.querySelector('.container'));
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+        .then(response => console.log(response))
 });
